0ae508d57b72c8e4421b75d5491d1aad5479cc06
asiskc <asiskc143@gmail.com>
2017-12-18 10:42:37 +0545

    equation

--- eq_stline.py
+++ eq_stline.py
@@ -3,7 +3,7 @@ def solve_for_y(x_values):
 
     for x in x_values:
         y = m*x + c
-        print("Value of y for m = 45, c = 0.5 and x = {:.2f} is {:.2f}.".format(x, y))
+        print("Values of y for m = 45, c = 0.5 and x = {:.2f} is {:.2f}.".format(x, y))
 
 
 x_value = [1, 2.3, 5.6, 7, 78]
